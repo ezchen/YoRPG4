2f32ccdfe00bfa7a814e7e48a519d070181c48c9
ezchen <echen2015@gmail.com>
2013-11-17 13:57:43 -0500

    Potion class

---
+++ Potion.java
@@ -0,0 +1,18 @@
+public class Potion {
+
+	protected int life;
+
+	public Potion() {
+		this.life = 0;
+	}
+
+	public Potion( int value ) {
+		this.life = value;
+	}
+
+	public int getLife() {
+		return this.life;
+	}
+
+
+}
