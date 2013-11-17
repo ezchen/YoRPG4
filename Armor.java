01be8c1b867cee1ac41210e0091ef14e9f9f484e
ezchen <echen2015@gmail.com>
2013-11-17 13:57:54 -0500

    Armor class

---
+++ Armor.java
@@ -0,0 +1,17 @@
+public class Armor {
+	
+	protected int defense;
+
+	public Armor() {
+		this.defense = 0;
+	}
+
+	public Armor( int newDefense ) {
+		this.defense = newDefense;
+	}
+
+	public int getDefense() {
+		return this.defense;
+	}
+
+}
