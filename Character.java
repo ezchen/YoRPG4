9cd9952d957e0f9a5b01bf3f61e343957d331cfd
ezchen <echen2015@gmail.com>
2013-11-17 13:57:12 -0500

    Character with equip/use methods

--- Character.java
+++ Character.java
@@ -4,13 +4,17 @@
 //2013-11-14
  
 public abstract class Character {
-	
+
+	//instance variables	
 	protected String name;
 	protected int life;
 	protected int strength;
 	protected int defense;
 	protected double attackRating;
+	protected Weapon weapon = new Weapon(0);
+	protected Armor armor = new Armor(0);
 
+	//getters
 	public String getName() {
 		return this.name;
 	}
@@ -18,7 +22,29 @@ public abstract class Character {
 	public int getDefense() {
 		return this.defense;
 	}
-	
+
+	public int getLife() {
+		return this.life;
+	}
+
+	//setters
+	public void setLife( int newLife ) {
+		this.life = newLife;
+	}
+
+	public void setStrength( int newStrength ) {
+		this.strength = newStrength;
+	}
+
+	public void setDefense( int newDefense ) {
+		this.defense = newDefense;
+	}
+
+	public void setAttackRating( int newAttackRating ) {
+		this.attackRating = newAttackRating;
+	}
+
+	//methods	
 	public boolean isAlive() {
 		return this.life > 0;
 	}
@@ -40,6 +66,31 @@ public abstract class Character {
 			character.lowerHP( damage );
 			return damage;
 		}
+	}
+
+	public double equip( Weapon newWeapon ) {
+		this.weapon = newWeapon;
+		return this.strength;
+	}
+
+	public double unEquipWeapon() {
+		this.weapon = new Weapon( 0 );
+		return this.attackRating;
+	}
+
+	public int equip( Armor newArmor ) {
+		this.armor = newArmor;
+		return this.defense;
+	}
+
+	public int unEquipArmor() {
+		this.armor = new Armor(0);
+		return this.defense;
+	}
+
+	public int use( Potion pot ) {
+		this.life = this.life + pot.getLife();
+		return this.life;
 	}
 
 	public abstract void specialize();
